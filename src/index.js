641a8e7521418629fcd35a345eb32f0f35b71f12
Eslim Daga Castro <esliim9811@gmail.com>
2022-05-09 19:50:55 -0500

    Envolviendo la aplicacion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/css/index.css";
+import { ThemeProvider } from "./context/ThemeContext";
+import Layout from "./template/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <BrowserRouter>
+        <Layout>
+          <App />
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
